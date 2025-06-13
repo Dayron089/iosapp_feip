6dc1e6e7aac3d23b9382d1fb960215401cb87dd4
Dmima <dimapelikh30@gmail.com>
2025-06-13 20:56:19 +1000

    Исправление пустого ProductDetailViewController.swift

--- console/ProductDetailViewController.swift
+++ console/ProductDetailViewController.swift
@@ -1,1 +1,10 @@
- 
+import UIKit
+
+class ProductDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Настройте ваш UI здесь
+    }
+
+} 
